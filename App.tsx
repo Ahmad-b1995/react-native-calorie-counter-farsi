b4966d295078ed5fa92f3be1a62f1eb16ba26e6d
ahmad baghereslami <ahmad.b1995@gmail.com>
2023-09-27 08:52:12 -0700

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+import { foods } from "./foods";
 
 export default function App() {
+  const [open, setOpen] = useState<boolean>(false);
+  const [value, setValue] = useState(null);
+  const [calorie, setCalorie] = useState<number>(0);
+  const [gram, setGram] = useState("");
+  const [items, setItems] = useState(foods);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <DropDownPicker
+        open={open}
+        value={value}
+        style={styles.picker}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+        placeholder={"انتخاب مواد غذایی"}
+        onChangeValue={(value) => setCalorie(value!)}
+        zIndex={1000}
+        dropDownContainerStyle={{
+          backgroundColor: "white",
+          zIndex: 1000,
+          elevation: 1000,
+        }}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={(value) => setGram(value)}
+        placeholder={"مقدار مواد غذایی (گرم)"}
+        value={gram}
+        keyboardType="numeric"
+      />
+      <Text style={styles.output}>{(+gram * calorie) / 100}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +46,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 100,
+    marginRight: 15,
+    marginLeft: 15,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: 25,
+  },
+  picker: {
+    backgroundColor: "#b7e8ff6b",
+    zIndex: 1000,
+  },
+  input: {
+    backgroundColor: "#b7e8ff6b",
+    width: "100%",
+    borderColor: "#000000b5",
+    borderWidth: 1,
+    height: 50,
+    borderRadius: 7,
+    padding: 15,
+    zIndex: 1,
+  },
+  output: {
+    marginTop: 20,
+    fontSize: 20,
   },
 });
---
+++ foods.ts
@@ -0,0 +1,167 @@
+export const foods = [
+  {
+    label: "سیب",
+    value: 78,
+  },
+
+  {
+    label: "تخم مرغ",
+    value: 143,
+  },
+  {
+    label: "سیب زمینی",
+    value: 77,
+  },
+  {
+    label: "سینه مرغ",
+    value: 165,
+  },
+  {
+    label: "ران مرغ",
+    value: 179,
+  },
+  {
+    label: "گوشت چرخ کرده گوساله",
+    value: 197,
+  },
+  {
+    label: "کرفس",
+    value: 16,
+  },
+  {
+    label: "پیاز",
+    value: 40,
+  },
+  {
+    label: "هویج",
+    value: 41,
+  },
+  {
+    label: "گوجه فرنگی",
+    value: 19,
+  },
+  {
+    label: "انگور قرمز و سبز",
+    value: 70,
+  },
+  {
+    label: "انجیر",
+    value: 74,
+  },
+  {
+    label: "موز",
+    value: 89,
+  },
+  {
+    label: "نان جو",
+    value: 313,
+  },
+  {
+    label: "عسل",
+    value: 304,
+  },
+  {
+    label: "پیاز",
+    value: 40,
+  },
+  {
+    label: "لیمو ترش",
+    value: 29,
+  },
+  {
+    label: "کدوسبز",
+    value: 17,
+  },
+  {
+    label: "بامیه سبز",
+    value: 40,
+  },
+  {
+    label: "فلفل سبز",
+    value: 40,
+  },
+  {
+    label: "سبزی خوردن",
+    value: 36,
+  },
+  {
+    label: "خیار",
+    value: 15,
+  },
+  {
+    label: "طالبی",
+    value: 40,
+  },
+  {
+    label: "خرما",
+    value: 282,
+  },
+  {
+    label: "تخم مرغ خام",
+    value: 143,
+  },
+  {
+    label: "ران مرغ",
+    value: 155,
+  },
+  {
+    label: "بالای ران مرغ",
+    value: 219,
+  },
+  {
+    label: "درام مرغ",
+    value: 160,
+  },
+  {
+    label: "کلم قرمز",
+    value: 31,
+  },
+  {
+    label: "شلیل",
+    value: 54,
+  },
+  {
+    label: "هندوانه",
+    value: 30,
+  },
+  {
+    label: "پنیر لیقوان",
+    value: 72,
+  },
+  {
+    label: "سیب",
+    value: 52,
+  },
+  {
+    label: "ترشی مخلوط",
+    value: 36,
+  },
+  {
+    label: "انگور ریش بابا",
+    value: 67,
+  },
+  {
+    label: "گلابی",
+    value: 58,
+  },
+  {
+    label: "ذرت",
+    value: 90,
+  },
+  {
+    label: "ترشی زیتون",
+    value: 145,
+  },
+  {
+    label: "ترب قرمز",
+    value: 16,
+  },
+  {
+    label: "ماکارونی",
+    value: 375,
+  },
+  {
+    label: "آلو قرمز",
+    value: 50,
+  },
+];
